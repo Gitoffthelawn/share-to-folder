da784279cddb9898945ac60b38a8c43a79132918
Alain Knaff <alain@knaff.lu>
2026-01-04 19:00:36 +0100

    Custom icon

---
+++ app/src/main/icons/convert.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+while read dir size ; do
+    convert -resize $size  app/src/main/icons/icon.svg app/src/main/res/$dir/ic_launcher.webp
+    convert -resize $size  app/src/main/icons/icon.svg app/src/main/res/$dir/ic_launcher_round.webp
+done <<EOF
+mipmap-mdpi 48
+mipmap-hdpi 72
+mipmap-xhdpi 96
+mipmap-xxhdpi 144
+mipmap-xxxhdpi 192
+EOF
