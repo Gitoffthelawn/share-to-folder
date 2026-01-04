8af7fa81c35b5ebb99bfebe423cd671627960aa3
Alain Knaff <alain@knaff.lu>
2026-01-04 19:39:16 +0100

    Make sure round icons do indeed have a round backdrop

--- app/src/main/icons/convert.sh
+++ app/src/main/icons/convert.sh
@@ -2,7 +2,7 @@
 
 while read dir size ; do
     convert -resize $size  app/src/main/icons/icon.svg app/src/main/res/$dir/ic_launcher.webp
-    convert -resize $size  app/src/main/icons/icon.svg app/src/main/res/$dir/ic_launcher_round.webp
+    convert -resize $size  app/src/main/icons/icon-round.svg app/src/main/res/$dir/ic_launcher_round.webp
 done <<EOF
 mipmap-mdpi 48
 mipmap-hdpi 72
