7dde327eea88ddcecce1763c3aa7f6107015d708
Alain Knaff <alain@knaff.lu>
2026-01-14 20:14:13 +0100

    Added license file, and license headers to kotlin sources

--- app/src/main/java/lu/knaff/alain/share_to_folder/MainActivity.kt
+++ app/src/main/java/lu/knaff/alain/share_to_folder/MainActivity.kt
@@ -1,5 +1,15 @@
 package lu.knaff.alain.share_to_folder
 
+/* This file is part of share-to-folder, an Android app to allow saving shared items to a folder
+ Copyright (C) 2025,2026 Alain Knaff
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+
 import java.net.URLDecoder
 
 import android.app.Activity
